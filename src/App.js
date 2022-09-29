efd9826eb2e8c4fd1bcc5cf9b9f55e3191a1b44e
AbdelrahmanLotfy <85247073+abdolotfy@users.noreply.github.com>
2022-09-29 15:18:39 +0200

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
-import React, { useState, useEffect } from "react";
-import * as BooksAPI from "./BooksAPI";
-import "./App.css";
-import Bookshelf from "./components/Bookshelf";
-import { useHistory } from "react-router-dom";
+import React from "react";
 
-const bookshelves = [
-  { title: "Currently Reading", shelfName: "currentlyReading" },
-  { title: "Want to Read", shelfName: "wantToRead" },
-  { title: "Read", shelfName: "read" }
-];
+import "./App.css";
+
 
 const App = () => {
- const [books, setBooks] = useState([]);
- const history = useHistory();
  
-// get all the books from the api 
-
-  useEffect(() => {
-    BooksAPI.getAll().then(booksFromApi => {
-      setBooks(booksFromApi);
-    });
-  }, []);
 
   return (
     <div className="app">
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {bookshelves.map((bookshelf, index) => (
-              <Bookshelf
-                key={index}
-                title={bookshelf.title}
-                books={
-                  books &&
-                  books.filter(
-                    book => book && book.shelf === bookshelf.shelfName
-                  )
-                }
-                setBooks={setBooks}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <button onClick={() => history.push("/search")}>Add a book</button>
-        </div>
-      </div>
+    
     </div>
   );
 };
 
 export default App;
+
